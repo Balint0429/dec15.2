8f0c9bf6a478e0d63146c536dfaea0950b298afe
Balint0429 <balintlaczko02@gmail.com>
2023-12-17 17:11:50 +0100

    dec17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { AlkatreszekComponent } from './alkatreszek/alkatreszek.component';
 import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BiciklikComponent } from './biciklik/biciklik.component';
+import { KiegeszitokComponent } from './kiegeszitok/kiegeszitok.component';
+import { RolunkComponent } from './rolunk/rolunk.component';
 
- 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    BiciklikComponent,
     AlkatreszekComponent,
+    KiegeszitokComponent,
+    RolunkComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    //NgModule,
-    //FormsModule,
-    //HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/rolunk/rolunk.component.ts
+++ src/app/rolunk/rolunk.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rolunk',
-  standalone: true,
-  imports: [],
   templateUrl: './rolunk.component.html',
   styleUrl: './rolunk.component.css'
 })
